8d87d665893be00f6f50051c08790dd0c6578564
Eric H. Smith <esmith167@gmail.com>
2021-03-02 15:34:15 -0500

    added validation'

--- semi_restful_tv_shows_app/models.py
+++ semi_restful_tv_shows_app/models.py
@@ -2,10 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+class TVshowsManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['addtitle']) < 2:
+            errors['addtitle'] = 'Title should at least me 2 characters.'
+        if len(postData['addnetwork']) < 3:
+            errors['addnetwork'] = 'Network should at least be 3 characters.'
+        if len(postData['adddescription']) < 10:
+            errors['description'] = 'Description should at least be 10 characters.'
+        return errors
+
+class TVshowsManagerEdit(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['edittitle']) < 2:
+            errors['edittitle'] = 'Title should at least me 2 characters.'
+        if len(postData['editnetwork']) < 3:
+            errors['editnetwork'] = 'Network should at least be 3 characters.'
+        if len(postData['editdescription']) < 10:
+            errors['editdescription'] = 'Description should at least be 10 characters.'
+        return errors
+
 class TVshows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=30)
     releaseDate = models.DateField(null=True, blank=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(blank=True, auto_now=True)
+    objects = TVshowsManager()
+    objects = TVshowsManagerEdit()
--- semi_restful_tv_shows_app/views.py
+++ semi_restful_tv_shows_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import TVshows
+from django.contrib import messages
 
 # Create your views here.
+
 def index(request):
     return redirect('/shows')
 
@@ -15,8 +17,14 @@ def showsnew(request):
     return render(request, 'showsnew.html')
 
 def showscreate(request):
-    newTVShow = TVshows.objects.create(title = request.POST['addtitle'], network = request.POST['addnetwork'], releaseDate = request.POST['addreleaseDate'], description = request.POST['adddescription'])
-    return redirect(f'/shows/{newTVShow.id}')
+    errors = TVshows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        newTVShow = TVshows.objects.create(title = request.POST['addtitle'], network = request.POST['addnetwork'], releaseDate = request.POST['addreleaseDate'], description = request.POST['adddescription'])
+        return redirect(f'/shows/{newTVShow.id}')
 
 def showsshow(request, showID):
     context = {
@@ -33,18 +41,29 @@ def showsedit(request, showID):
     return render(request, 'showsedit.html', context)
 
 def showsedited(request, showID):
+    print(showID)
+    print('*******************')
+    errors = TVshows.objects.basic_validator(request.POST)
     editTVShow = TVshows.objects.get(id = showID)
-    # editTVShow = TVshows.objects.update(title = request.POST['edittitle'], network = request.POST['editnetwork'], releaseDate = request.POST['editreleaseDate'], description = request.POST['editdescription']
-    editTVShow.title = request.POST['edittitle']
-    editTVShow.network = request.POST['editnetwork']
-    editTVShow.releaseDate = request.POST['editreleaseDate']
-    editTVShow.description = request.POST['editdescription']
-    editTVShow.save()
-    context = {
-        "show" : TVshows.objects.get(id = showID),
-        "show_DB": TVshows.objects.all()
-    }
-    return redirect(f'/shows/{showID}', context)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        print(showID)
+        print('*******************')
+        return redirect(f'/shows/{editTVShow.id}/edit')
+    else:
+        #editTVShow = TVshows.objects.get(id = showID)
+        # editTVShow = TVshows.objects.update(title = request.POST['edittitle'], network = request.POST['editnetwork'], releaseDate = request.POST['editreleaseDate'], description = request.POST['editdescription']
+        editTVShow.title = request.POST['edittitle']
+        editTVShow.network = request.POST['editnetwork']
+        editTVShow.releaseDate = request.POST['editreleaseDate']
+        editTVShow.description = request.POST['editdescription']
+        editTVShow.save()
+        context = {
+            "show" : TVshows.objects.get(id = showID),
+            "show_DB": TVshows.objects.all()
+        }
+        return redirect(f'/shows/{showID}', context)
 
 def showsdelete(request, showID):
     showToDelete = TVshows.objects.get(id = showID)
